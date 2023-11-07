e5090a5a4c5d1fa5b70efab58a8866b8f2e3c216
Ranis Minsafin <ranis@MacBook-Air-Ranis.local>
2023-11-07 18:37:47 +0300

    first step

---
+++ Team00/src/ex00/Game/src/main/java/edu/school21/console/game/app/IllegalParametersException.java
@@ -0,0 +1,8 @@
+package edu.school21.console.game.app;
+
+public class IllegalParametersException extends RuntimeException {
+
+    public IllegalParametersException(String message) {
+        super(message);
+    }
+}
