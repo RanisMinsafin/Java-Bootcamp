82ec19a3fa0a3ed1b94fbd2d667743b3aa127fad
Ranis Minsafin <bobbyzz999@gmail.com>
2023-11-23 01:12:44 +0300

    ex00: start

---
+++ Day06/ex00/Tests/src/test/java/edu/school21/numbers/NumberWorkerTest.java
@@ -0,0 +1,41 @@
+package edu.school21.numbers;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class NumberWorkerTest {
+    private NumberWorker numberWorker;
+
+    @BeforeEach
+    void setUp() {
+        numberWorker = new NumberWorker();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2,3,5,7"})
+    void isPrimeForPrimes(int number) {
+        boolean isPrime = numberWorker.isPrime(number);
+        Assertions.assertTrue(isPrime);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"4,6,8,9"})
+    void isPrimeForNotPrimes(int number) {
+        boolean isPrime = numberWorker.isPrime(number);
+        Assertions.assertFalse(isPrime);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 1, -100, -999"})
+    void isPrimeForIncorrectNumbers(int invalidNumber) {
+        Assertions.assertThrows(IllegalNumberException.class, ()->numberWorker.isPrime(invalidNumber));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void testDigitsSum(int number, int expectedNumber){
+        Assertions.assertEquals(numberWorker.digitsSum(number), expectedNumber);
+    }
+}
