6ccc4d587750e6af9c4c3561342b9aaa7cc6ab5a
Dario Gladys <dariogla@pr-n6.kzn.21-school.ru>
2024-01-03 19:28:03 +0300

    delete Team00

--- Team00/src/ex00/Game/README.txt
+++
@@ -1,3 +0,0 @@
-mvn package
-
-java -cp 'target/classes:target/dependency/*' edu.school21.console.game.app.Main --enemiesCount=10 --wallsCount=10 --size=30 --profile=production
--- Team00/src/ex00/Game/src/java/edu/school21/console/game/app/IllegalParametersException.java
+++
@@ -1,8 +0,0 @@
-package edu.school21.console.game.app;
-
-public class IllegalParametersException extends RuntimeException {
-
-    public IllegalParametersException(String message) {
-        super(message);
-    }
-}
